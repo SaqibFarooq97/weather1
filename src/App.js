fbb538e0a69f6b3468501d38b7388e8c52f68e33
unknown <sakibfarooq@gmail.com>
2023-05-13 18:00:36 +0530

    addeed functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cityName, setCityName] = useState("Srinagar");
+  const [inputText, setInputText] = useState("");
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=189271b827844bff7388350c44848615&units=metric`
+    )
+      .then((res) => {
+        console.log(res)
+        if (res.status === 200) {
+          console.log(res.json)
+          error && setError(false);
+          return res.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        console.log(data)
+        setData(data);
+      })
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, [cityName, error]);
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      setCityName(e.target.value);
+      setInputText("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className="bg_img">
+      {/* {!loading ? (
+        <> */}
+      <input
+        variant="filled"
+        label="Search location"
+        className="input"
+        error={error}
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        onKeyDown={handleSearch}
+      />
+      <h1 className="city">{data?.name}</h1>
+      <div className="group">
+            <img
+              src={`http://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`}
+              alt=""
+            />
+            <h1>{data?.weather[0].main}</h1>
+          </div>
+
+      <h1 className="temp">{data?.main.temp.toFixed()} °C</h1>
+
+      {/* <Slide direction="right" timeout={800} in={!loading}> */}
+      <div className="box_container">
+        <div className="box">
+          <p>Humidity</p>
+          <h1>{data?.main.humidity.toFixed()}%</h1>
+        </div>
+
+        <div className="box">
+          <p>Wind</p>
+          <h1>{data?.wind.speed.toFixed()} km/h</h1>
+        </div>
+
+        <div className="box">
+          <p>Feels Like</p>
+          <h1>{data?.main.feels_like.toFixed()} °C</h1>
+        </div>
+      </div>
+      {/* </Slide> */}
+      {/* </>
+      ) : ( */}
+      {/* <CircularProgress /> */}
+      {/* )} */}
     </div>
   );
 }
